d414d71b40b8b51b4c30ac488eff9897bc4e546b
Andrey Belskiy <anb@smena.space>
2024-02-28 07:48:03 +0500

    classes dependencies

---
+++ fastapistuff/006_classes_dependencies.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Depends
+from typing import Annotated
+
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    # Those parameters are what FastAPI will use to "solve" the dependency
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+
+@app.get("/pineapples/")
+async def get_pineapples(commons: Annotated[CommonQueryParams, Depends()]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
