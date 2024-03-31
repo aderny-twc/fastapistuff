c815aeb32f51e1735ae25f69676c98cd4a1ab40a
Andrey Belskiy <anb@smena.space>
2024-03-31 07:35:07 +0500

    Configure params for async db sessions

--- db/database.py
+++ db/database.py
@@ -1,20 +1,17 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./img_api.db"
+SQLITE_DATABASE_URL = "sqlite+aiosqlite:///./img_api.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+engine = create_async_engine(
+    SQLITE_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionlLocal = sessionmaker(autoflush=False, bind=engine)
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False, autoflush=False)
 
 Base = declarative_base()
 
 
-def get_db():
-    db = SessionlLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
